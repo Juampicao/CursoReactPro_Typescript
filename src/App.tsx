8533ea7274969853c497523f72f1e0884789f485
juampicao <juanpiparada@gmail.com>
2023-01-04 18:41:54 -0300

    use-axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { MapsApp } from "./MapsApp"
-
+import Prueba from "./component/Prueba"
 const App = () => {
   return (
     <>
       <h1>Hola desde App</h1>
-      <MapsApp />
+      <Prueba/>
     </>
   )
 }
